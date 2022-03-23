5480a48a92f71f797afee187f8a6749909244302
Jan <janvanbuel@hotmail.com>
2022-03-23 10:04:59 +0100

    make cicd resource unique

--- extra_CI_CD/main.tf
+++ extra_CI_CD/main.tf
@@ -1,5 +1,9 @@
 resource "aws_ssm_parameter" "first_cicd_resource" {
-  name  = "/cicd-test"
+  name  = "/terraform_workshop/provider_config/${random_pet.name.id}"
   type  = "String"
   value = "Hello Terraform! This resource has been created by CICD"
+}
+
+resource "random_pet" "name" {
+  length = 2
 }
