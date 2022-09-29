df69b21c346f91b36cc21ea8a0e905f3ac3d2bed
Jan <janvanbuel@hotmail.com>
2022-09-29 09:43:06 +0200

    add permissions to stub

--- 02_remote_backend/provider.tf
+++ 02_remote_backend/provider.tf
@@ -8,6 +8,9 @@ terraform {
   }
   backend "s3" {
     // TO DO: configure this correctly after setting up the remote backend
+    profile  = "academy"
+    role_arn = "arn:aws:iam::338791806049:role/exercise_02_role"
+
   }
 }
 
